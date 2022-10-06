5b7e362a6fc66b5610f7159ac23a215c8599927a
Anker7 <lshutaro1679@gmail.com>
2022-10-06 21:56:03 +0000

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  root  'home#index' 
   resources :users
   resources :recipes do
     resource :likes, only: [:create, :destroy]
